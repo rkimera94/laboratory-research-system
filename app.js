9c96ac86a2303ba3fedac792163f31a48501e1f6
Ronald Kimera <rkimera2013@gmail.com>
2020-11-25 12:11:26 +0300

    add routes folder

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res, next) => {
-  console.log(req.url);
-  console.log(req.method);
-  res.send("I am the home route");
-});
-app.post("/", (req, res, next) => {});
+const ReagentsRoute = require("./Routes/ReagentsRoute");
 
 app.listen(3040, () => {
   console.log("Serve started at prot 3040");
