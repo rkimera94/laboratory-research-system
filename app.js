f626d3648d60cc67cc1930ea42a223befb9887a5
Ronald Kimera <rkimera2013@gmail.com>
2020-11-25 11:01:01 +0300

    branc 1

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("i am running... and port coming");
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    if (req.method === "GET") {
+      console.log("i am a get method");
+    }
+    res.write("iam listening at ome page");
+    res.end();
+  } else if (req.url === "/another") {
+    res.write("iam listening at another route ");
+    res.end();
+  } else {
+    res.write("I am listening");
+    res.end();
+  }
+});
+server.listen(3040, () => {
+  console.log("server started on port 3040...");
+});
