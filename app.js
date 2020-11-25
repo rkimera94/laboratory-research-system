1d3644bce2dc5c53f948f6a10c639439f779c5e1
Ronald Kimera <rkimera2013@gmail.com>
2020-11-25 11:19:10 +0300

    test routes

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    if (req.method === "GET") {
-      console.log("i am a get method");
-    }
-    res.write("iam listening at ome page");
-    res.end();
-  } else if (req.url === "/another") {
-    res.write("iam listening at another route ");
-    res.end();
-  } else {
-    res.write("I am listening");
-    res.end();
-  }
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  console.log(req.url);
+  console.log(req.method);
+  res.send("I am the home route");
 });
-server.listen(3040, () => {
-  console.log("server started on port 3040...");
+app.post("/", (req, res, next) => {});
+
+app.listen(3040, () => {
+  console.log("Serve started at prot 3040");
 });
