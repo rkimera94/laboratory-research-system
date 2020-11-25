e72dd44cd2841646327f342f7c1c7f9ee91873a3
Ronald Kimera <rkimera2013@gmail.com>
2020-11-25 12:41:00 +0300

    add routes

--- Routes/ReagentsRoute.js
+++ Routes/ReagentsRoute.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/reagents", (req, res, next) => {});
+router.get("/", (req, res, next) => {
+  res.send("NDA store");
+});
+router.post("/", (req, res, next) => {
+  res.send("reagent created");
+});
+router.get("/:id", (req, res, next) => {
+  res.send("fetch reagent by id");
+});
 
+router.patch("/:id", (req, res, next) => {
+  res.send("updated reagent by id");
+});
+
+router.delete("/:id", (req, res, next) => {
+  res.send("deleted reagent by id");
+});
 module.exports = router;
