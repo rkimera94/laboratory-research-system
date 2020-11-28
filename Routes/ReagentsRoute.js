b535b73e5ba56799b8754681db4a804e9ddd5b2f
Ronald Kimera <rkimera2013@gmail.com>
2020-11-28 10:24:58 +0300

    error handling

--- Routes/ReagentsRoute.js
+++ Routes/ReagentsRoute.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const createError = require("http-errors");
+const mongoose = require("mongoose");
 const router = express.Router();
 const Reagent = require("../models/reagentsModel");
 
@@ -46,9 +48,17 @@ router.get("/:id", async (req, res, next) => {
   try {
     const reagent = await Reagent.findById(id);
     // or findone() method
+    if (!reagent) {
+      throw createError(404, "reagent not found");
+    }
     res.send(reagent);
   } catch (error) {
     console.log(error.message);
+    if (error instanceof mongoose.CastError) {
+      next(createError(400, "invalid id"));
+      return;
+    }
+    next(error);
   }
 });
 
@@ -58,9 +68,17 @@ router.patch("/:id", async (req, res, next) => {
     const updates = req.body;
     const options = { new: true };
     const result = await Reagent.findByIdAndUpdate(id, updates, options);
+    if (!result) {
+      throw createError(404, "reagent not found");
+    }
     res.send(result);
   } catch (error) {
+    if (error instanceof mongoose.CastError) {
+      return next(createError(400, "Invalid reagent id"));
+      return;
+    }
     console.log(error.message);
+    next(error);
   }
 });
 
@@ -68,9 +86,17 @@ router.delete("/:id", async (req, res, next) => {
   const id = req.params.id;
   try {
     const result = await Reagent.findByIdAndDelete(id);
+    if (!result) {
+      throw createError(404, "reagent not found");
+    }
     res.send(result);
   } catch (error) {
     console.log(error.message);
+    if (error instanceof mongoose.CastError) {
+      next(createError(400, "invalid id"));
+      return;
+    }
+    next(error);
   }
 });
 module.exports = router;
