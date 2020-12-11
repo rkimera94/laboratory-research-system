5c76a08051a91324f51ff4f725361df7908f5701
Ronald Kimera <rkimera2013@gmail.com>
2020-12-12 02:46:48 +0300

    add joi validation

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const createError = require("http-errors");
+const { authSchema } = require("../helpers/validation_schema");
 
 const User = require("../models/Users.model");
 
 router.post("/register", async (req, res, next) => {
   console.log(req.body);
   try {
-    const { email, password } = req.body;
-    if (!email || !password) throw createError.BadRequest();
+    // const { email, password } = req.body;
+    // (!email || !password) throw createError.BadRequest();
+    const result = await authSchema.validateAsync(req.body);
 
-    const doesExist = await User.findOne({ email: email });
-    if (doesExist) throw createError.Conflict(`${email} is already registered`);
+    const doesExist = await User.findOne({ email: result.email });
+    if (doesExist)
+      throw createError.Conflict(`${result.email} is already registered`);
 
-    const user = new User({ email, password });
+    const user = new User(result);
     const savedUser = await user.save();
     res.send(savedUser);
   } catch (error) {
+    if (error.isJoi === true) error.status = 422;
     next(error);
   }
 });
